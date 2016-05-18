f07062c17e8fd37518ff074361b0401cae6ffed8
Patrick <pmurphy2@wpi.edu>
2016-05-18 09:20:50 -0400

    Adding Source so that Toby can work on stuff

--- src/GRIP/Source.java
+++ src/GRIP/Source.java
@@ -1,14 +1,19 @@
 package GRIP;
 
-import java.util.Properties;
+
 
 public class Source {
-	protected Properties properties;
-	
-	public Source(Properties properties){
-		this.properties = properties;
+	protected String name;
+	protected String value;
+	protected int sourceNumber;
+	public Source(String name, String value, int number){
+		this.name = name;
+		this.value = value;
+		this.sourceNumber = number;
 	}
 	
-
+	public boolean hasOutput(OutputType type, int value, int socket){
+		return type==OutputType.SOURCE && value == this.sourceNumber;
+	}
 
 }
